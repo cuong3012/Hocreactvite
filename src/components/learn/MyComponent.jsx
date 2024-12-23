f860c3a4313ee3de2c00764a13d1f9d8281f054c
cuong hoc react <cuong30122000@gmail.com>
2024-12-23 23:38:31 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,11 @@
+import './style.css'
 const MyComponent = () => {
     return (
-        <div> Cuong & my component xys</div>
+        <>
+            <div> Cuong & my component xys</div>
+            <div className="child" style={{ borderRadius: "10px" }}> child</div>
+        </>
+
     );
 }
 export default MyComponent;
