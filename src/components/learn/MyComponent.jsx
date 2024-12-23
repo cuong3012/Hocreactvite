223a5019bee74697b159411f98d59fef6fc62f44
cuong hoc react <cuong30122000@gmail.com>
2024-12-23 23:54:48 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,18 @@
 import './style.css'
 const MyComponent = () => {
+
+    // const tentui = "cuong123";
+    // const tentui = 1;
+    // const tentui = [1,2,3,4];
+    const tentui = {
+        name: "Cuong",
+        age: 39
+    };
+
     return (
         <>
-            <div> Cuong & my component xys</div>
+            <div> {JSON.stringify(tentui)} & my component xys</div>
+            <div>{console.log("ABSCS")}</div>
             <div className="child" style={{ borderRadius: "10px" }}> child</div>
         </>
 
