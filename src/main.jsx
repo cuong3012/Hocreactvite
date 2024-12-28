d67a2087ddba1bfc00c0b634031a9c935f10130c
cuong hoc react <cuong30122000@gmail.com>
2024-12-28 12:20:11 +0700

    video 48

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,17 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/users",
+        element: <UserPage />,
+      },
+      {
+        path: "/products",
+        element: <ProductPage />,
+      },
+
+    ]
   },
 
   {
@@ -25,14 +36,7 @@ const router = createBrowserRouter([
     path: "/register",
     element: <RegisterPage />,
   },
-  {
-    path: "/users",
-    element: <UserPage />,
-  },
-  {
-    path: "/products",
-    element: <ProductPage />,
-  },
+
 
 ]);
 
